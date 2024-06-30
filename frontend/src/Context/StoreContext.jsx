ee2804b22c15bbbd4ecb44f8c22d771b342832b3
Rahul12347 <2019rahul.maurya@ves.ac.in>
2024-06-30 17:46:01 +0530

    Second Commit

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -1,9 +1,9 @@
+import axios from "axios";
 import { createContext, useEffect, useState } from "react";
 export const StoreContext = createContext(null);
-import axios from "axios"
 const StoreContextProvider = (props) => {
   const[cartItems,setCartItems]= useState({})
-  const url="http://localhost:4000";
+  const url="https://food-delivery-mernstack-main.onrender.com";
   const[token,setToken]=useState("")
   const[food_list,Setfood_list]=useState([])
   const addToCart= async(itemId)=>{
